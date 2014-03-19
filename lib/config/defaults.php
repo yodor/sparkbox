eab1601d370b950344eec8938bb9b77e2f183ef4
Todor Gyumyushev <yodor1@gmail.com>
2014-03-19 14:22:32 +0200

    fix defaults deployment folder

--- lib/config/defaults.php
+++ lib/config/defaults.php
@@ -12,7 +12,7 @@ define ("INSTALL_PATH", $install_path);
 $lib_path = $install_path."/lib";
 define ("LIB_PATH" , $lib_path);
 
-$site_root = str_replace($install_path, "", $install_path);
+$site_root = str_replace($_SERVER["DOCUMENT_ROOT"], "", $install_path);
 //app/site deployment - HTTP accessible
 define ("SITE_ROOT", $site_root."/");
 
