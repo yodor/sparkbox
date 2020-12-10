bbf4e5b517e7f5998da38f6d5a51d46b0e3c0bfc
Todor Gyumyushev <yodor1@gmail.com>
2020-12-10 14:36:35 +0200

    update
    Signed-off-by: Todor Gyumyushev <yodor1@gmail.com>

--- lib/templates/admin/AdminLoginForgotPassword.php
+++ lib/templates/admin/AdminLoginForgotPassword.php
@@ -21,7 +21,7 @@ class ForgotPasswordProcessor extends FormProcessor
         $users = new AdminUsersBean();
 
         $random_pass = Authenticator::RandomToken(8);
-        $fpm = new ForgotPasswordMailer($email, $random_pass, SITE_DOMAIN . LOCAL . "admin/login.php");
+        $fpm = new ForgotPasswordMailer($email, $random_pass, SITE_DOMAIN . ADMIN_LOCAL . "/login.php");
         $db = DBConnections::Factory();
         try {
             $db->transaction();
