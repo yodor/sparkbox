f0eea76d30ed850f6e68ef8b0dc12bc44b90c1b7
Todor Gyumyushev <yodor1@gmail.com>
2021-09-03 16:18:12 +0300

    updates
    Signed-off-by: Todor Gyumyushev <yodor1@gmail.com>

--- lib/components/ClosureComponent.php
+++ lib/components/ClosureComponent.php
@@ -3,9 +3,20 @@
 class ClosureComponent extends Component
 {
     protected $closure = null;
+
     public function __construct(Closure $callback)
     {
         parent::__construct();
+        $this->closure = $callback;
+    }
+
+    public function getCloser() : Closure
+    {
+        return $this->closure;
+    }
+
+    public function setClosure(Closure $callback)
+    {
         $this->closure = $callback;
     }
 
