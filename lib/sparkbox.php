60e439f3fe82252c676426e85664e6569d78a7d8
Todor Gyumyushev <yodor1@gmail.com>
2021-09-19 18:32:49 +0300

    updates redirect to https
    Signed-off-by: Todor Gyumyushev <yodor1@gmail.com>

--- lib/sparkbox.php
+++ lib/sparkbox.php
@@ -11,6 +11,18 @@
 
 if (!$install_path) throw new Exception("Install path is not defined");
 
+if (isset($GLOBALS["FORCE_HTTPS"]) && $GLOBALS["FORCE_HTTPS"]==0) {
+
+}
+else {
+    if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
+        $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+        header('HTTP/1.1 301 Moved Permanently');
+        header('Location: ' . $location);
+        exit;
+    }
+}
+
 include_once("utils/SparkGlobals.php");
 $defines = SparkGlobals::Instance();
 
