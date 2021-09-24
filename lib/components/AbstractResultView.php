4005cdc098993556f986e6fd1b0ce7a1f25a970a
Todor Gyumyushev <yodor1@gmail.com>
2021-09-24 18:59:31 +0300

    updates
    Signed-off-by: Todor Gyumyushev <yodor1@gmail.com>

--- lib/components/AbstractResultView.php
+++ lib/components/AbstractResultView.php
@@ -153,16 +153,20 @@ abstract class AbstractResultView extends Component implements IDataIteratorRend
             return;
         }
 
-        $select = clone $this->iterator->select;
-        $select->setMode(SQLSelect::SQL_CALC_FOUND_ROWS);
-        $select->setMode(SQLSelect::SQL_CACHE);
-        $select->limit = 1;
+        $orderFilter = $this->paginator->prepareOrderFilter($this->default_order);
+        $pageFilter = $this->paginator->preparePageFilter();
+
+        $this->iterator->select->combine($pageFilter);
+        $this->iterator->select->combine($orderFilter);
+
+        $this->iterator->select->setMode(SQLSelect::SQL_CALC_FOUND_ROWS);
+        $this->iterator->select->setMode(SQLSelect::SQL_CACHE);
+
+        echo "Final SQL: ".$this->iterator->select->getSQL();
+
+        $pageResults = $this->iterator->exec();
 
         $db = $this->iterator->getDB();
-        if (!$db->query($select->getSQL())) {
-            debug("Error selecting all number of records: " . $db->getError() . " SQL: " . $select->getSQL());
-            throw new Exception($db->getError());
-        }
         $res = $db->query("SELECT FOUND_ROWS() as total");
         $result = $db->fetch($res);
         $this->total_rows = (int)$result["total"];
@@ -170,26 +174,8 @@ abstract class AbstractResultView extends Component implements IDataIteratorRend
 
         $this->paginator->calculate($this->total_rows, $this->items_per_page);
 
-
-        $orderFilter = $this->paginator->prepareOrderFilter($this->default_order);
-
-        $pageFilter = $this->paginator->preparePageFilter();
-
-        if ($this->paginators_enabled) {
-            $this->iterator->select->combine($pageFilter);
-            $this->iterator->select->combine($orderFilter);
-        }
-
-        $this->iterator->select->setMode(SQLSelect::SQL_CACHE);
-        //echo "Final SQL: ".$this->iterator->select->getSQL();
-
-        //number of results for the current page - limited by $this->items_per_page
-        $pageResults = $this->iterator->exec();
-
         if($this->paginators_enabled & AbstractResultView::PAGINATOR_TOP) {
-
             $this->paginator_top->render();
-
         }
     }
 
