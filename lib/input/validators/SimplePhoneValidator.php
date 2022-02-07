8ce21f33f61d0560ea70ef0a532dbd8c3d776476
Todor Gyumyushev <yodor1@gmail.com>
2022-02-07 15:57:56 +0200

    updates require numeric phone number for fast orders
    Signed-off-by: Todor Gyumyushev <yodor1@gmail.com>

---
+++ lib/input/validators/SimplePhoneValidator.php
@@ -0,0 +1,34 @@
+<?php
+include_once("input/DataInput.php");
+include_once("input/validators/IInputValidator.php");
+
+class SimplePhoneValidator implements IInputValidator
+{
+
+    protected $allow_plus;
+
+    public function __construct(bool $allow_plus = TRUE)
+    {
+        $this->allow_plus = $allow_plus;
+    }
+    /**
+     * @param DataInput $input
+     * @throws Exception
+     */
+    public function validate(DataInput $input)
+    {
+
+        $value = $input->getValue();
+
+        if (strlen(trim($value)) == 0 && $input->isRequired()) {
+            throw new Exception("Input numeric value");
+        }
+        $value = str_replace(" ", "", $value);
+
+        if (!preg_match("/^\+?\d+$/",  $value)) throw new Exception("Input numeric value");
+
+    }
+
+}
+
+?>
