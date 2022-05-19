3bd75d2e14e6991e34627d45fee02ac37f8046f6
Todor Gyumyushev <yodor1@gmail.com>
2022-05-20 01:40:03 +0300

    jsonitemview loading more results without paginator buttons
    Signed-off-by: Todor Gyumyushev <yodor1@gmail.com>

---
+++ lib/components/JSONItemView.php
@@ -0,0 +1,96 @@
+<?php
+include ("components/ItemView.php");
+
+class ItemViewResponder extends JSONResponder
+{
+
+    /**
+     * @var JSONItemView
+     */
+    protected $view;
+
+    public function __construct(string $cmd, JSONItemView $view)
+    {
+        parent::__construct($cmd);
+        $this->view = $view;
+    }
+
+    public function _renderItems(JSONResponse $resp)
+    {
+        debug("... start");
+        $this->view->processResponse($resp);
+        debug("... finish");
+    }
+
+}
+
+class JSONItemView extends ItemView
+{
+    /**
+     * @var ItemViewResponder
+     */
+    protected $responder;
+
+    protected $button;
+
+    protected $container;
+
+    public function __construct(?IDataIterator $itr = NULL)
+    {
+        parent::__construct($itr);
+        $this->responder = new ItemViewResponder("ItemViewResponder", $this);
+        $this->enablePaginators(AbstractResultView::PAGINATOR_TOP);
+
+        $this->container = new Container();
+        $this->container->setClassName("loader");
+        $this->button = new ColorButton();
+        $this->button->setContents("<label>".tr("Show More")."</label>"."<div class='progress-bar'><div class='circle border'></div></div>");
+
+        $this->button->setAttribute("onClick", "loadMoreResults(this)");
+        $this->button->setName("fetchResults");
+
+        $this->container->append($this->button);
+    }
+
+    public function requiredStyle() : array
+    {
+        $arr = parent::requiredStyle();
+        $arr[] = SPARK_LOCAL . "/css/JSONItemView.css";
+        return $arr;
+    }
+
+    public function requiredScript() : array
+    {
+        $arr = parent::requiredScript();
+        $arr[] = SPARK_LOCAL . "/js/JSONItemView.js";
+        return $arr;
+    }
+
+    public function startRender()
+    {
+        AbstractResultView::startRender();
+        $this->viewport->startRender();
+    }
+
+    public function finishRender()
+    {
+        $this->viewport->finishRender();
+
+        if ($this->paginator->getCurrentPage() < $this->paginator->getPagesTotal()-1) {
+            $this->container->render();
+        }
+
+        AbstractResultView::finishRender();
+    }
+
+    public function processResponse(JSONResponse $resp)
+    {
+        $this->processIterator();
+        ob_start();
+        $this->renderResults();
+        $resp->html = ob_get_contents();
+        ob_end_clean();
+    }
+
+}
+?>
---
+++ sparkfront/js/JSONItemView.js
@@ -0,0 +1,35 @@
+function loadMoreResults(button_source)
+{
+    let itemView = $(button_source).parents(".JSONItemView").first();
+    let viewPort = itemView.children(".viewport").first();
+
+    //currentPage = index
+    //pagesTotal = count
+    let currentPage = parseInt(itemView.attr("page")) ;
+    let pagesTotal = parseInt(itemView.attr("pagesTotal"));
+
+    if (isNaN(currentPage)) currentPage = 0;
+
+    if (currentPage < pagesTotal) {
+        currentPage++;
+
+        let request = new JSONRequest();
+        request.setResponder("ItemViewResponder");
+        request.setFunction("renderItems");
+        request.setParameter("page", currentPage);
+
+        request.onSuccess = function (result) {
+
+            viewPort.append(result.json_result.html);
+            itemView.attr("page", currentPage);
+            $(button_source).removeAttr("working", "");
+
+        };
+        $(button_source).attr("working", "");
+        request.start();
+    }
+    if (currentPage == pagesTotal-1) {
+        $(button_source).remove();
+    }
+
+}
