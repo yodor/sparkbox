f81057cb97cf573b5a43e2acef6adeade39a1b99
Todor Gyumyushev <yodor1@gmail.com>
2023-02-24 16:09:24 +0200

    add comment
    Signed-off-by: Todor Gyumyushev <yodor1@gmail.com>

--- lib/utils/DataParameter.php
+++ lib/utils/DataParameter.php
@@ -4,17 +4,22 @@ include_once("utils/URLParameter.php");
 class DataParameter extends URLParameter
 {
 
-    public function __construct(string $name, string $value = "")
+    /**
+     * Construct the url query parameter
+     * @param string $name  Set the url query parameter name to '$name' ex. ?$name=
+     * @param string $field Data result field key name. If empty use '$name' as field key name
+     */
+    public function __construct(string $name, string $field = "")
     {
 
-        if (!$value) {
+        if (!$field) {
             $this->field = $name;
         }
         else {
-            $this->field = $value;
+            $this->field = $field;
         }
 
-        parent::__construct($name, $value);
+        parent::__construct($name, $field);
 
     }
 
