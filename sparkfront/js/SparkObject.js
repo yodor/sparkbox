5f79cb4f8c25222c3b62681c8747b06b4bdf01e8
Todor Gyumyushev <yodor1@gmail.com>
2023-03-02 17:36:34 +0200

    update: implement observer callbacks
    Signed-off-by: Todor Gyumyushev <yodor1@gmail.com>

--- sparkfront/js/SparkObject.js
+++ sparkfront/js/SparkObject.js
@@ -1,10 +1,39 @@
 class SparkObject {
 
     constructor() {
-
-
+        this.observers = [];
     }
 
+    /**
+     * add callback function
+     * @param func {Function}
+     */
+    addObserver(func) {
+        this.observers.push(func);
+    }
 
+    /**
+     * remove callback function
+     * @param func {Function}
+     */
+    removeOvserver(func) {
+        this.observers = this.observers.filter(
+            function (item) {
+                if (item !== func) {
+                    return item;
+                }
+            }
+        );
+    }
 
+    /**
+     * Notify subscribed observer callbacks
+     * @param sparkEvent {SparkEvent}
+     */
+    notify(sparkEvent) {
+        //var scope = thisObj || window;
+        this.observers.forEach(function (item) {
+            item(sparkEvent);
+        });
+    }
 }
