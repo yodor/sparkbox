804991358afa4a8cbea61984374af54a6c719894
Todor Gyumyushev <yodor1@gmail.com>
2024-08-24 22:07:28 +0300

    cleanups

--- lib/responders/json/ImageUploadResponder.php
+++ lib/responders/json/ImageUploadResponder.php
@@ -39,61 +39,29 @@ class ImageUploadResponder extends UploadControlResponder implements IPhotoRende
     public function getHTML(FileStorageObject &$object, string $field_name)
     {
 
-        //TODO:prepare other style contents for files. render files as alternating rows icon, filename , type, size, X
-
         debug("...");
 
         $filename = $object->getFileName();
-
         $mime = $object->getMIME();
-
         $uid = $object->getUID();
-
-        $itemID = $object->id;
-
-        $itemClass = $object->className;
 
         debug("UID:$uid filename:$filename mime:$mime");
 
-        //gc_collect_cycles();
-
-        if (!($object instanceof FileStorageObject)) {
-            throw new Exception("Incorrect storage object received");
-        }
-
-        //construct image data in row and pass to ImageScaler to create a temporary thumbnail of the uploaded image.
+        //create a temporary thumbnail of the uploaded image
         $scaler = new ImageScaler($this->width, $this->height);
+        $scaler->process($object->getData(), $object->getLength(), $mime);
 
-        $mime = $object->getMIME();
-
-        //data is null during ajax upload. image data can be retrieved from tempName file.
-        if ($object->getData()) {
-            $scaler->process($object->getData(), $object->getLength(), $mime);
-        }
-        else {
-            $scaler->process(file_get_contents($object->getTempName()), filesize($object->getTempName()), $mime);
-        }
-
-        //temporary resize for base64_encode returned in ajax response
         $image_data = "data:$mime;base64," . base64_encode($scaler->getData());
 
         ob_start();
-
-        $item = new StorageItem();
-        $item->className = $object->className;
-        $item->id = $object->id;
-
-        echo "<div class='Element' tooltip='$filename' itemID='$itemID' itemClass='$itemClass'>";
+        echo "<div class='Element' tooltip='$filename' >";
         echo "<img class='thumbnail' src='$image_data'>";
         echo "<span class='remove_button' action='Remove'>X</span>";
         echo "<input type=hidden name='uid_{$field_name}[]' value='$uid' >";
         echo "</div>";
-
         $html = ob_get_contents();
-
         ob_end_clean();
 
-        //return array("name" => $filename, "uid" => $uid, "mime" => $mime, "html" => $html,);
         return $html;
     }
 
