ac8d56113558e2e5250cb67d4bd0cec5544290b3
Todor Gyumyushev <yodor1@gmail.com>
2024-08-24 22:11:29 +0300

    fixes: do not set value for certain input types

--- lib/input/renderers/InputFieldTag.php
+++ lib/input/renderers/InputFieldTag.php
@@ -5,12 +5,30 @@ include_once("input/renderers/InputField.php");
 abstract class InputFieldTag extends InputField
 {
 
+    protected $skip_value_types = array("file");
+
     protected function processInputAttributes()
     {
         parent::processInputAttributes();
 
-        $field_value = mysql_real_unescape_string($this->input->getValue());
-        $this->setInputAttribute("value", $field_value);
+        $process_value = true;
+
+        if ($this->haveInputAttribute("type")) {
+
+            $type = $this->getInputAttribute("type");
+
+            if (in_array($type, $this->skip_value_types)) {
+                $process_value = false;
+                debug("Disable value setting for type: ".$type);
+            }
+
+        }
+
+        if ($process_value) {
+            $field_value = mysql_real_unescape_string($this->input->getValue());
+            $this->setInputAttribute("value", $field_value);
+        }
+
 
     }
 
