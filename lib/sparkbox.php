4a85fccd93ad5df3881cf5f65bccbbe6aa2ac434
Todor Gyumyushev <yodor1@gmail.com>
2024-08-25 03:24:54 +0300

    use http/2

--- lib/sparkbox.php
+++ lib/sparkbox.php
@@ -22,7 +22,7 @@ if (isset($GLOBALS["FORCE_HTTPS"]) && $GLOBALS["FORCE_HTTPS"]==0) {
 else {
     if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
         $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-        header('HTTP/1.1 301 Moved Permanently');
+        header('HTTP/2 301 Moved Permanently');
         header('Location: ' . $location);
         exit;
     }
