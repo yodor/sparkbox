33ea716955f63f46db14e2f6531ae1c5184e576c
Todor Gyumyushev <yodor1@gmail.com>
2024-09-06 13:44:13 +0300

    update: make EmptyUserAgent as ratelimited

--- lib/ratelimit.php
+++ lib/ratelimit.php
@@ -1,4 +1,5 @@
 <?php
+
 function storePacket($fh, array $data)
 {
     flock($fh, LOCK_EX);
@@ -22,65 +23,72 @@ function readPacket($fh) : array
 }
 function rateCheck(string $install_path)
 {
+    $names = explode("|", REQUEST_THROTTLE_USERAGENT);
 
-    $userAgent = $_SERVER['HTTP_USER_AGENT'];
+    $emptyUserAgent="EmptyUserAgent";
+    $userAgent = "";
+    if (isset($_SERVER['HTTP_USER_AGENT'])) {
+        $userAgent = trim($_SERVER['HTTP_USER_AGENT']);
+    }
 
-    if (!empty($userAgent)) {
-        $names = explode("|", REQUEST_THROTTLE_USERAGENT);
+    if (strlen($userAgent)<1) {
+        $userAgent = $emptyUserAgent;
+        $names[] = $emptyUserAgent;
+    }
 
-        foreach ($names as $nameMatch) {
+    foreach ($names as $nameMatch) {
 
-            if (!str_contains($userAgent, $nameMatch)) continue;
+        if (!str_contains($userAgent, $nameMatch)) continue;
 
-            $installID = hash('xxh3', $install_path);
+        $installID = hash('xxh3', $install_path);
 
-            //temporary file for each installation and userAgent name
-            $tmpFile = sys_get_temp_dir() . "/" . $nameMatch . "-" . $installID;
+        //temporary file for each installation and userAgent name
+        $tmpFile = sys_get_temp_dir() . "/" . $nameMatch . "-" . $installID;
 
-            if ($fh = fopen($tmpFile, 'c+')) {
+        if ($fh = fopen($tmpFile, 'c+')) {
 
-                $data = readPacket($fh);
+            $data = readPacket($fh);
 
-                $lastTimeFloat = (float)$data[0]; //last access from this userAgent
-                $connCount = (int)$data[1]; //connection count
+            $lastTimeFloat = (float)$data[0]; //last access from this userAgent
+            $connCount = (int)$data[1]; //connection count
 
-                $timeNow = microtime(true);
-                $timeDiff = $timeNow - $lastTimeFloat;
+            $timeNow = microtime(true);
+            $timeDiff = $timeNow - $lastTimeFloat;
 
-                $connCount++;
+            $connCount++;
 
-                //number of connection exceeded for the amount of time given
-                if ($connCount > REQUEST_THROTTLE_CONNCOUNT && $timeDiff < REQUEST_THROTTLE_SECONDS) {
-                    // bail with http 429 Too Many Requests
-                    header($_SERVER["SERVER_PROTOCOL"] . ' 429');
+            //number of connection exceeded for the amount of time given
+            if ($connCount > REQUEST_THROTTLE_CONNCOUNT && $timeDiff < REQUEST_THROTTLE_SECONDS) {
+                // bail with http 429 Too Many Requests
+                header($_SERVER["SERVER_PROTOCOL"] . ' 429');
 //                    $data[0] = (float)$lastTimeFloat;
 //                    $data[1] = (int)$connCount;
 //                    storePacket($fh, $data);
-                    fclose($fh);
-                    exit;
-                }
-                if ($timeDiff > REQUEST_THROTTLE_SECONDS) {
-                    //reset connection count
-                    $data[0] = (float)$timeNow;
-                    $data[1] = (int)1;
-                    storePacket($fh, $data);
-                    fclose($fh);
-                    return;
-                }
-
-                //allow this request, store the current connection count
-                $data[0] = (float)$lastTimeFloat;
-                $data[1] = (int)$connCount;
-                storePacket($fh, $data);
                 fclose($fh);
-            }
-            else {
-                header($_SERVER["SERVER_PROTOCOL"] . ' 430');
                 exit;
             }
-        }
+            if ($timeDiff > REQUEST_THROTTLE_SECONDS) {
+                //reset connection count
+                $data[0] = (float)$timeNow;
+                $data[1] = (int)1;
+                storePacket($fh, $data);
+                fclose($fh);
+                return;
+            }
 
+            //allow this request, store the current connection count
+            $data[0] = (float)$lastTimeFloat;
+            $data[1] = (int)$connCount;
+            storePacket($fh, $data);
+            fclose($fh);
+        }
+        else {
+            header($_SERVER["SERVER_PROTOCOL"] . ' 430');
+            exit;
+        }
     }
+
+
 }
 
 rateCheck($install_path);
