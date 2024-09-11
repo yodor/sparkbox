a35fe9e93c5bdfcdbe8c8b27dab3db2d5ee8f93b
Todor Gyumyushev <yodor1@gmail.com>
2024-09-11 22:25:04 +0300

    updates/enable multi-value insert

--- lib/sql/SQLInsert.php
+++ lib/sql/SQLInsert.php
@@ -16,9 +16,61 @@ class SQLInsert extends SQLStatement
         $sql = $this->type . " " . $this->from;
         $sql .= "(" . implode(",", array_keys($this->set)) . ")";
         $sql .= " VALUES ";
-        $sql .= "(" . implode(",", array_values($this->set)) . ")";
+
+
+        $keys = array_keys($this->set);
+        $values = array_values($this->set);
+        //debug("Values contents: ".print_r($values, true));
+
+        if (is_array($values[0])) {
+            //if value is array count should be equal for each element in the set
+            $values_count = count($values[0]);
+
+            foreach ($keys as $key) {
+                if (!is_array($this->set[$key]) || count($this->set[$key])!=$values_count) {
+                    throw new Exception("Values count should be equal for each column in the set");
+                }
+            }
+
+            $values_sql = array();
+            for ($idx = 0; $idx < $values_count; $idx++) {
+                $row = array();
+                //foreach column in the set
+                foreach ($values as $value) {
+                    $row[] = $value[$idx];
+                }
+                $values_sql[] = $this->prepareValues($row);
+            }
+            $sql.= implode(",", $values_sql);
+        }
+        else {
+            $sql .= $this->prepareValues($values);
+        }
 
         return $sql;
+    }
+
+    /**
+     * SQL Encode values
+     * @param array $values
+     * @return string
+     */
+    protected function prepareValues(array $values) : string
+    {
+        return "(" . implode(",", $values) . ")";
+    }
+
+    /**
+     * Append value for multi-value insert to column '$column'
+     * No quoting or escaping is done
+     * @param string $column
+     * @param string $value
+     * @return void
+     */
+    public function setAppend(string $column, string $value) : void
+    {
+        $this->set[$column][] = $value;
+
     }
 
 }
