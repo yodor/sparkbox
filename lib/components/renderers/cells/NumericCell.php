0ed355e1ed40818a6f3e0007e9099b15b72e1886
Todor Gyumyushev <yodor1@gmail.com>
2024-11-22 18:17:46 +0200

    refactoring/cleanups

--- lib/components/renderers/cells/NumericCell.php
+++ lib/components/renderers/cells/NumericCell.php
@@ -9,17 +9,14 @@ class NumericCell extends TableCell
     public function __construct(string $format = "%01.2f")
     {
         parent::__construct();
-
         $this->format = $format;
-
         $this->addClassName("Numeric");
     }
 
     public function setData(array $data) : void
     {
-
+        parent::setData($data);
         $this->setContents(sprintf($this->format, $this->getContents()));
-
     }
 
 }
