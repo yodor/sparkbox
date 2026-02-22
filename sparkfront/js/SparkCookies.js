ef5f5a0261fe486b373e926c97d46d2d05a3c648
Todor Gyumyushev <yodor1@gmail.com>
2026-02-22 16:17:54 +0200

    refactoring

--- sparkfront/js/SparkCookies.js
+++ sparkfront/js/SparkCookies.js
@@ -3,10 +3,10 @@ class SparkCookies extends SparkObject
     constructor() {
         super();
         this.defaultCookies = {
-            "ad_user_data": "denied",
-            "ad_personalization": "denied",
-            "ad_storage": "granted",
-            "analytics_storage": "granted"
+            "ad_user_data": "denied", //Whether user data can be sent to Google for advertising purposes
+            "ad_personalization": "denied", //Whether data can be used for ad personalization
+            "ad_storage": "granted", //Whether ad cookies can be read/written
+            "analytics_storage": "granted" //Whether analytics cookies can be read/written
         };
         this.acceptedCookies = {
             "ad_user_data": "denied",
@@ -39,10 +39,10 @@ class SparkCookies extends SparkObject
         let event = new SparkEvent(SparkEvent.GTM_EVENT, null);
 
         if (isAccepted) {
-            event.gtm = {command:"consent", type:"update", parameters:this.defaultCookies};
+            event.gtm = {command:"consent", type:"update", parameters:this.acceptedCookies};
         }
         else {
-            event.gtm = {command:"consent", type:"update", parameters:this.acceptedCookies};
+            event.gtm = {command:"consent", type:"update", parameters:this.defaultCookies};
         }
         document.dispatchEvent(event);
     }
