72a371a94dc5dec4b33b46317ce6d29ec0eaf781
Todor Gyumyushev <yodor1@gmail.com>
2026-02-26 23:02:44 +0200

    refactoring

---
+++ lib/objects/events/TemplateEvent.php
@@ -0,0 +1,20 @@
+<?php
+include_once("objects/SparkEvent.php");
+
+class TemplateEvent extends SparkEvent
+{
+    const string CONFIG_CREATED = "CONFIG_CREATED";
+    const string CONTENT_CREATED = "CONTENT_CREATED";
+
+    const string CONTENT_INITIALIZED = "CONTENT_INITIALIZED";
+
+    const string CONTENT_INSERTED = "CONTENT_INSERTED";
+
+    const string CONTENT_INPUT_PROCESSED = "CONTENT_INPUT_PROCESSED";
+    const string MENU_CREATED = "MENU_CREATED";
+
+    public function __construct(string $name = "", ?SparkObject $source = null)
+    {
+        parent::__construct($name, $source);
+    }
+}
