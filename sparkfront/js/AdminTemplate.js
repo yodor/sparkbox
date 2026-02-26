e9afd5ff3ebfaa296a0436775a26ef91580da6a8
Todor Gyumyushev <yodor1@gmail.com>
2026-02-27 01:19:31 +0200

    admin v2

---
+++ sparkfront/js/AdminTemplate.js
@@ -0,0 +1,46 @@
+class HelpFetcher {
+    constructor() {
+        this.request = new JSONRequest();
+        this.request.setResponder("AdminHelpResponder");
+        this.request.setFunction("fetch");
+        this.target = null;
+    }
+
+    /**
+     *
+     * @param elm {HTMLElement}
+     */
+    fetch(elm) {
+
+        const path = elm.getAttribute('path');
+        const sink = "." + elm.getAttribute('sink');
+
+        this.target = document.querySelector(sink);
+
+        this.request.setParameter("path", path);
+
+        this.request.onSuccess = (result) => {
+            this.target.innerHTML = result.response.message;
+
+            elm.setAttribute("onClick", "javascript:document.helpFetcher.toggle();");
+
+            this.toggle();
+        };
+
+        this.request.start();
+
+    }
+    toggle()
+    {
+        const element = this.target;
+        if (getComputedStyle(element).display === 'none') {
+            // Restore previous or use preferred value
+            element.style.display = 'block';
+        } else {
+            // Remember current display value and hide
+            element.style.display = 'none';
+        }
+    }
+}
+
+document.helpFetcher = new HelpFetcher();
