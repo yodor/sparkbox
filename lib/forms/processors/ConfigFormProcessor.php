5162f82fdff63ebc3d511b28c98c89e00cd76495
Todor Gyumyushev <yodor1@gmail.com>
2026-03-03 15:22:32 +0200

    refactoring

--- lib/forms/processors/ConfigFormProcessor.php
+++ lib/forms/processors/ConfigFormProcessor.php
@@ -4,13 +4,10 @@ include_once("forms/processors/FormProcessor.php");
 class ConfigFormProcessor extends FormProcessor implements IValueTransactor
 {
 
-    protected array $values = array();
-
     public function __construct()
     {
         parent::__construct();
     }
-
 
     protected function processImpl(InputForm $form) : void
     {
@@ -20,32 +17,23 @@ class ConfigFormProcessor extends FormProcessor implements IValueTransactor
 
         $input_names = $form->inputNames();
 
-
         foreach ($input_names as $name) {
-
             $input = $form->getInput($name);
-
             $input->getProcessor()->transactValue($this);
-
-
         }
-
-        foreach ($this->values as $name => $value) {
-            if ($value instanceof StorageObject) {
-                $value = $value->serializeDB();
-            }
-            $this->bean->set($name, $value);
-        }
-
     }
 
     /**
      * Add value to this transactor values. Will be commited with the main transaction
      * @param string $key
-     * @param $val
+     * @param $value
      */
     public function appendValue(string $key, $val) : void
     {
-        $this->values[$key] = $val;
+        if ($val instanceof StorageObject) {
+            $val = $val->serializeDB();
+        }
+        $this->bean->set($key, $val);
     }
+
 }
