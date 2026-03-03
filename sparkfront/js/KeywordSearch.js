211bc73bc66d608f6110f9426e7665b94f42da2e
Todor Gyumyushev <yodor1@gmail.com>
2026-03-03 15:13:07 +0200

    refactoring keyword search

---
+++ sparkfront/js/KeywordSearch.js
@@ -0,0 +1,58 @@
+class KeywordSearch extends Component {
+    constructor() {
+        super();
+        this.class = ".KeywordSearch";
+    }
+
+    initialize() {
+        super.initialize();
+
+        this.element.addEventListener('submit', (event)=> {
+            return this.onSubmit(event);
+        });
+    }
+
+    submitURL(event) {
+        event.preventDefault();
+
+        let form = event.target;
+        let url = new URL(form.action || window.location.href);
+        let formData = new FormData(form);
+
+        formData.forEach((value, key) => {
+            url.searchParams.set(key, value);
+        });
+
+        window.location.href = url.href;
+    }
+    onSubmit(event) {
+
+        const submitter = event.submitter;
+
+        try {
+            if (!submitter) throw "";
+
+            const buttonName = submitter.name;
+            const buttonValue = submitter.value;
+
+            //const form = document.forms.KeywordSearchForm;
+            const query = this.element.keyword.value;
+            const minChars = parseInt(this.element.keyword.getAttribute("min-chars"));
+
+            if (submitter.value === "search" && query.length < minChars) throw "Input search term";
+            if (submitter.value === "clear" && query.length < 1) throw "";
+
+        }
+        catch (e) {
+            event.preventDefault();
+            if (e) showAlert(e);
+            return false;
+        }
+    }
+}
+onPageLoad(function() {
+
+    let ks = new KeywordSearch();
+    ks.initialize();
+
+})
